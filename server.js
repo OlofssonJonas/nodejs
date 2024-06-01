6cc90bd0714a22b3c4836c37cfac9c75412138e8
Jonas Olofsson <jonas.olofsson81@hotmail.com>
2024-06-01 16:38:49 +0200

    Fixed login with cookies

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express = require("express");
 const connectDB = require("./src/db");
 const app = express();
 const Router = require("./src/router");
+const cookieSession = require("cookie-session");
 
 app.use(express.json());
+app.use(cookieSession({
+    name: 'session',
+    secret: 's3cr3t',
+    maxAge: 1000 * 60,
+    httpOnly: true,
+    sameSite: true
+}));
 
 app.use("/api", Router);
 
--- src/router.js
+++ src/router.js
@@ -2,37 +2,54 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const UserModel = require("./model");
+const cookieSession = require("../server");
 
-router.get('/get', async(req, res) => {
-    const name = await UserModel.find();
-    res.status(200).json(name);
+
+router.get("/get", async (req, res) => {
+  const name = await UserModel.find();
+  res.status(200).json(name);
 });
 
-router.post('/register', async(req, res) => {
-    try {
-        const { name, email, password } = req.body;
-        const hashedPassword = await bcrypt.hash(password, 10);
-        console.log(hashedPassword);
-        const existingUser = await UserModel.findOne({email: req.body.email});
-        if(!existingUser) {
-            const newUser = await UserModel.create({name: name, email: email, password: hashedPassword});
-            console.log('17', newUser);
-             res.status(201).json({message: 'User created', user: newUser});
-        }else{
-            res.status(400).json({message: 'User already exist'})
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({message: 'Server error'})
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const existingUser = await UserModel.findOne({ email: req.body.email });
+    if (!existingUser) {
+      const newUser = await UserModel.create({
+        name: name,
+        email: email,
+        password: hashedPassword,
+      });
+      res.status(201).json({ message: "User created", user: newUser });
+    } else {
+      res.status(400).json({ message: "User already exist" });
     }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
-router.post("/signin", (req, res) => {
-    try {
-        console.log(req.body);
-    } catch (error) {
-        
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const existingUser = await UserModel.findOne({ email: req.body.email });
+    const hashedPassword = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+    if (hashedPassword) {
+        res.cookie("user", existingUser.email);
+      res.status(200).json({ message: "You are logged in" });
+    } else {
+      res.status(400).json({ message: "Wrong credentials" })
     }
-})
+} catch (error) {
+    res.status(500).json({ message: "Server error" });
+    console.log(error);
+}
+});
 
 module.exports = router;
